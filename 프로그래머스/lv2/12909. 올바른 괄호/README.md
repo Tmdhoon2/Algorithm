82d1a67e6b133ee385be618727d8292dba6fb2b4
정승훈 <102812085+Tmdhoon2@users.noreply.github.com>
2023-09-18 19:19:27 +0900

    [level 2] Title: 올바른 괄호, Time: 5.62 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/README.md
+++ 프로그래머스/lv2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 62.6 MB, 시간: 17.43 ms
+메모리: 52.8 MB, 시간: 5.62 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,20 +1,12 @@
-import java.util.ArrayList;
-
 class Solution {
     boolean solution(String s) {
-        ArrayList<Character> arrayList = new ArrayList<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            arrayList.add(s.charAt(i));
-        }
-
         int leftExpected = 0;
         int rightExpected = 0;
 
-        if(arrayList.get(arrayList.size() - 1) == '(') return false;
+        if(s.charAt(s.length() - 1) == '(') return false;
 
-        for (int i = 0; i < arrayList.size(); i++) {
-            switch (arrayList.get(i)) {
+        for (int i = 0; i < s.length(); i++) {
+            switch (s.charAt(i)) {
                 case '(': {
                     rightExpected += 1;
                     leftExpected -= 1;
