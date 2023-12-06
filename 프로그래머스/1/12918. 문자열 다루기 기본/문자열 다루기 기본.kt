7004f80b8c8ec98fea684ae2e7662dca6cde6861
정승훈 <102812085+Tmdhoon2@users.noreply.github.com>
2023-12-06 18:55:39 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 2.84 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.kt
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.kt
@@ -1,13 +1,5 @@
 class Solution {
     fun solution(s: String): Boolean {
-    if(s.length != 4 && s.length != 6){
-        return false
-    }
-    for(i in s.indices){
-        if(s[i].toInt() !in 48..57){
-            return false
-        }
-    }
-    return true
+    return Regex("[0-9]{4}|[0-9]{6}").matches(s)
 }
 }
